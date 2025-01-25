6fda134ea631ab8f329e8a527d5b6222e4c5929f
Arshia Aravinthan <arshia.a.2006@gmail.com>
2025-01-25 14:05:52 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ function register() {
   let userInput = document.getElementById("name-input").value;
   console.log(userInput);
 
-  fetch("http://localhost:5000/add-realtor/" + userInput).then((response) => {
+  fetch("https://537e-169-234-82-73.ngrok-free.app/" + userInput).then((response) => {
     getDatabaseDataAndShowOnWebsite();
   });
 }
 
 function getDatabaseDataAndShowOnWebsite() {
-  fetch("http://localhost:5000/")
+  fetch("https://537e-169-234-82-73.ngrok-free.app/")
     //we get data in a hjumbled binary mess, we convert into a readable understanble format
-    .then((response) => response.json())
     //
     .then((response) => {
       document.getElementById("display").innerHTML = JSON.stringify(
@@ -19,5 +18,3 @@ function getDatabaseDataAndShowOnWebsite() {
     });
 }
 
-
-print("hello world ")
