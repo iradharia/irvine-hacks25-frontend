70ce1b94bc81824b986f6fee24d062a9eb886089
iradharia <108150018+iradharia@users.noreply.github.com>
2025-01-24 23:10:38 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function register() {
+  let userInput = document.getElementById("name-input").value;
+  console.log(userInput);
+
+  fetch("http://localhost:5000/add-realtor/" + userInput).then((response) => {
+    getDatabaseDataAndShowOnWebsite();
+  });
+}
+
+function getDatabaseDataAndShowOnWebsite() {
+  fetch("http://localhost:5000/")
+    //we get data in a hjumbled binary mess, we convert into a readable understanble format
+    .then((response) => response.json())
+    //
+    .then((response) => {
+      document.getElementById("display").innerHTML = JSON.stringify(
+        response.realtors
+      );
+    });
+}
